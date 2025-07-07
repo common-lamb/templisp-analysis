bec98b6d690478457324801b317bd5b596294426
lamb-qwey <lambqwey@gmail.com>
2025-07-07 17:37:28 -0500

    add: exper

--- analysis.lisp
+++ analysis.lisp
@@ -8,7 +8,7 @@
 
 ;;;; ==================================== create package
 
-;;                                         ; imports
+                                        ; imports
 (ql:quickload :qlot)
 (ql:quickload :coalton)
 (ql:quickload :fset)
@@ -42,9 +42,9 @@
 (setf *print-level* 100)
 (setf *print-length* 50)
 
-;;;; ==================================== Globals
+;;;; ==================================== contents
 
-;;;; ==================================== X
+;; global variables
 
 ;;;; run-all-reports
 ;;;; multiplex file-name-parts/stats-calls into experimental runs
@@ -102,10 +102,308 @@
 ;;;; format stat-test text
 ;;;; save report to disk
 ;;;; optional display in buffer
+;;;; ==================================== global variables
 
+(defparameter *input-root* #P"/home/holdens/tempdata/predictions1percent/"
+              "Pathname to a dir containing tiff files. With naming format:<TRAIT>.tiff OR PREDICTED_<TRAIT>_<objective>_<MODEL>.tiff")
+
+(defparameter *output-root* #P"/home/holdens/tempdata/OPanalysis/")
+
+(defparameter *models* '(
+                         "TSAI"
+                         "GBM"
+                         )
+  "List of strings naming models which are to be compared")
+(defparameter *traits-reg* '(
+                             "HEIGHT-CM"
+                             ;; "AREA"
+                             ;; "DENSITY"
+                             ;; "DIAMETER"
+                             ;; "SBLOTCH-LMH"
+                             ;; "SBLOTCH-RATING"
+                             ;; "STEM-WEIGHT"
+                             ;; "WEIGHT"
+                             )
+  "List of strings of traits for which the prediction objective was 'regression'")
+(defparameter *traits-cat* '(
+                             "BARLEY-WHEAT"
+                             ;; "HULLED"
+                             ;; "ROWS"
+                             ;; "SBLOTCH-LMH"
+                             ;; "SBLOTCH-RATING"
+                             )
+  "List of strings of traits for which the prediction objective was 'multiclass'")
+
+(defparameter *stats-reg-describe-true*'(
+                                         test-histogram
+                                         test-mean
+                                         ;;&&&
+                                         ))
+(defparameter *stats-cat-describe-true*'(
+                                         test-barchart
+                                         ;;&&&
+                                         ))
+(defparameter *stats-reg-describe-pred*'(
+                                         test-histogram
+                                         test-mean
+                                         ;;&&&
+                                         ))
+(defparameter *stats-cat-describe-pred*'(
+                                         test-barchart
+                                         ;;&&&
+                                         ))
+(defparameter *stats-reg-compare-pred*'(
+                                        test-R2
+                                        test-residual
+                                        ;;&&&
+                                        ))
+(defparameter *stats-cat-compare-pred* '(
+                                         test-F1
+                                         test-confusion-matrix
+                                         ;;&&&
+                                         ))
+(defparameter *stats-reg-compare-models* '(
+                                           test-anova-reg
+                                           ;;&&&
+                                           ))
+(defparameter *stats-cat-compare-models* '(
+                                           test-anova-cat
+                                           ;;&&&
+                                           ))
+
+;;;; ==================================== Functions
+
+;;;; run-all-reports
+;;;; multiplex file-name-parts/stats-calls into experimental runs
+;;;; validate all filename file properties
+;;;; validate all filename statscalls allowableness
+;;;; validate file set geospatial properties match
+;;;; call run-report for each
+
+(defun run-all-reports (&key (show nil))
+  (-<> (filename-parts show)
+    (experiment-dictionary <> show)
+    ;; (mapcar validate-files <>)
+    ;; (mapcar validate-calls <>)
+    ;; (mapcar validate-geospatial <>)
+    ;; (mapcar run-report <>)
+    ))
+
+(defun filename-parts(&optional (show nil))
+  "Create a list of filename components"
+  (let ((experiments '()))
+    (dolist (trait *traits-reg*)
+      ;; build a p list of lists for the experiment
+      (let ((experiment (list :trait (list trait)
+                              :objective (list "regression")
+                              :models *models*)))
+        (push experiment experiments)))
+    (dolist (trait *traits-cat*)
+      ;; build a p list of lists for the experiment
+      (let ((experiment (list :trait (list trait)
+                              :objective (list "multiclass")
+                              :models *models*)))
+        (push experiment experiments)))
+    ;; clean up and return
+    (let ((clean-experiments (remove-duplicates (reverse experiments) :test #'equal)))
+      (when show
+          (dolist (i clean-experiments)
+            (format t "~&~A" i)))
+      clean-experiments)))
+
+(defun experiment-dictionary (filename-parts &optional (show nil))
+  "completes the dictionary of experiment definitions"
+  (when show (print "experiment-dictionary"))
+  (let ((experiments '()))
+    (dolist (fp filename-parts)
+      (let* ((objective (first (getf fp :objective)))
+             (models (getf fp :models))
+             (to-append (cond ((string= objective "regression")
+                               (append *stats-reg-describe-true*
+                                       *stats-reg-describe-pred*
+                                       *stats-reg-compare-pred*
+                                       (when (>= (length models) 2)
+                                         *stats-reg-compare-models*)))
+                              ((string= objective "multiclass")
+                               (append *stats-cat-describe-true*
+                                       *stats-cat-describe-pred*
+                                       *stats-cat-compare-pred*
+                                       (when (>= (length models) 2)
+                                         *stats-cat-compare-models*)))
+                              (t (error "Unknown objective: ~A~&" objective)))))
+        (let ((experiment (copy-list fp)))
+          (setf (getf experiment :stats) to-append)
+          (push experiment experiments))))
+    (setf experiments (nreverse experiments))
+    (when show
+      (dolist (exp experiments)
+        (format t "~&~S~%" exp)))
+    experiments))
+
+(defun experiment-dictionary (filename-parts &optional (show nil))
+  "Completes the dictionary of experiment definitions"
+  (let ((completed-experiments '()))
+    (dolist (experiment filename-parts)
+      (let* ((objective (first (getf experiment :objective)))
+             (models (getf experiment :models))
+             (tests (cond
+                      ((string= objective "regression")
+                       (append *stats-reg-describe-true*
+                               *stats-reg-describe-pred*
+                               *stats-reg-compare-pred*
+                               (when (>= (length models) 2)
+                                 *stats-reg-compare-models*)))
+                      ((string= objective "multiclass")
+                       (append *stats-cat-describe-true*
+                               *stats-cat-describe-pred*
+                               *stats-cat-compare-pred*
+                               (when (>= (length models) 2)
+                                 *stats-cat-compare-models*)))
+                      (t (error "Unknown objective: ~A" objective))))
+             (completed-experiment (copy-list experiment)))
+        (setf (getf completed-experiment :tests) tests)
+        (push completed-experiment completed-experiments)))
+
+    (setf completed-experiments (nreverse completed-experiments))
+
+    (when show
+      (dolist (exp completed-experiments)
+        (format t "~&~S~%" exp)))
+
+    completed-experiments))
+
+
+(run-all-reports)
+
+
+  ;;with a list whose elements are like: (TRAIT (HEIGHT-CM) OBJECTIVE (regression) MODELS (TSAI))
+  ;; add a list :tests
+  ;;to which we will add the stats function lists
+  ;; if objective is regression add: *stats-reg-describe-true* *stats-reg-describe-pred* *stats-reg-compare-pred*
+  ;; if objective is multiclass add: *stats-cat-describe-true* *stats-cat-describe-pred* *stats-cat-compare-pred*
+  ;; if length of models is 2 add the correct objective of: *stats-reg-compare-models* *stats-cat-compare-models*
+
+  ;;&&&
+
+
+
+(run-all-reports :show t )
 
 ;;;; ==================================== FIN
+;;;; ==================================== build
 
+#|
+                # set both objective and model_type are None to export only trait map
+    # exports = [{'trait_name': 'HEIGHT', 'objective': None, 'model_type': None},
+    #            {'trait_name': 'HEIGHT', 'objective': 'regression', 'model_type': 'TSAI'},
+    #            {'trait_name': 'HEIGHT', 'objective': 'multiclass', 'model_type': 'TSAI'},
+    #            {'trait_name': 'HEIGHT', 'objective': 'regression', 'model_type': 'GBM'},
+    #            {'trait_name': 'HEIGHT', 'objective': 'multiclass', 'model_type': 'GBM'}]
+
+    models = [
+        "TSAI",
+        "GBM"]
+    # objectives = ["regression", "multiclass"]
+    traits_reg = [
+        'HEIGHT-CM',
+        'AREA',
+        'DENSITY',
+        'DIAMETER',
+        'SBLOTCH-LMH',
+        'SBLOTCH-RATING',
+        'STEM-WEIGHT',
+        'WEIGHT',
+    ]
+    traits_cat = [
+        'BARLEY-WHEAT',
+        'HULLED',
+        'ROWS',
+        'SBLOTCH-LMH',
+        'SBLOTCH-RATING',
+    ]
+
+    exports = []
+
+    # deduplicate if repeats in reg and cat
+    for m in models:
+        for t in traits_reg:
+            trait = {'trait_name': t, 'objective': None , 'model_type': None }
+            prediction =  {'trait_name': t, 'objective': 'regression' , 'model_type': m }
+            if trait not in exports:
+                exports.append(trait)
+            if prediction not in exports:
+                exports.append(prediction)
+        for t in traits_cat:
+            trait = {'trait_name': t, 'objective': None , 'model_type': None }
+            prediction = {'trait_name': t, 'objective': 'multiclass', 'model_type': m }
+            if trait not in exports:
+                exports.append(trait)
+            if prediction not in exports:
+                exports.append(prediction)
+
+|#
+
+
+
+
+(defparameter *temp-experiment*
+  '(
+    (:trait "HEIGHT")
+    (:objectives () )
+    (:models ())
+
+
+
+    ))
+#|
+&&&
+I have a python export that uses this list builder, how can I emulate it in common lisp?
+
+
+
+To emulate the Python list builder in Common Lisp, you can use a combination of `loop` and `append`. Here's how you can create a similar structure:
+
+```lisp
+(defun build-exports ()
+  (let ((exports '()))
+    (loop for m in *models* do
+      (loop for t in *traits-reg* do
+        (let ((trait `((:trait-name ,t :objective nil :model-type nil)))
+              (prediction `((:trait-name ,t :objective "regression" :model-type ,m))))
+          (setf exports (append exports
+                                (remove-duplicates
+                                 (append trait prediction)
+                                 :test #'equal)))))
+      (loop for t in *traits-cat* do
+        (let ((trait `((:trait-name ,t :objective nil :model-type nil)))
+              (prediction `((:trait-name ,t :objective "multiclass" :model-type ,m))))
+          (setf exports (append exports
+                                (remove-duplicates
+                                 (append trait prediction)
+                                 :test #'equal))))))
+    exports))
+
+(defparameter *exports* (build-exports))
+```
+
+This function does the following:
+
+1. It initializes an empty list `exports`.
+2. It loops through each model in `*models*`.
+3. For each model, it loops through traits in `*traits-reg*` and `*traits-cat*`.
+4. For each trait, it creates a trait map and a prediction map.
+5. It appends these maps to the `exports` list, removing duplicates.
+
+The resulting `*exports*` will be a list of plists (property lists) that correspond to the dictionaries in the Python version.
+
+You can then access `*exports*` to get the full list of exports.
+
+|#
+
+#|
+&&&
+
+|#
                                         ; X
 ;;;; ==================================== X
 ;;; ===================================== X
@@ -117,6 +415,8 @@
 ;; py4cl2:*config* ;; WARN: this triggers the company auto complete hang on lab linux
 
 ;; (setf (config-var pycmd) python3") ; set one field
+
+
 
                                         ; ensure version and sys.path is same as python in cli
 (py:pyversion-info)    ; fails if python command is not resolved in system
