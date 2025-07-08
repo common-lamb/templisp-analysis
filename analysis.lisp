568a25cd8f6f39db45d8a7262ca87013afd32322
lamb-qwey <lambqwey@gmail.com>
2025-07-08 15:57:45 -0500

    add: validate-globals

--- analysis.lisp
+++ analysis.lisp
@@ -108,8 +108,10 @@
                                         ; file locations
 (defparameter *input-root* #P"/home/holdens/tempdata/predictions1percent/"
               "Pathname to a dir containing tiff files. With naming format:<TRAIT>.tiff OR PREDICTED_<TRAIT>_<objective>_<MODEL>.tiff")
+;; &&& other input files and subdirs
 
 (defparameter *output-root* #P"/home/holdens/tempdata/OPanalysis/")
+;; &&& output subdirs
 
                                         ; input tif filename components
 (defparameter *models* '(
@@ -119,21 +121,21 @@
   "List of strings naming models which are to be compared")
 (defparameter *traits-reg* '(
                              "HEIGHT-CM"
-                             "AREA"
-                             "DENSITY"
-                             "DIAMETER"
-                             "SBLOTCH-LMH"
-                             "SBLOTCH-RATING"
-                             "STEM-WEIGHT"
-                             "WEIGHT"
+                             ;; "AREA"
+                             ;; "DENSITY"
+                             ;; "DIAMETER"
+                             ;; "SBLOTCH-LMH"
+                             ;; "SBLOTCH-RATING"
+                             ;; "STEM-WEIGHT"
+                             ;; "WEIGHT"
                              )
   "List of strings of traits for which the prediction objective was 'regression'")
 (defparameter *traits-cat* '(
                              "BARLEY-WHEAT"
-                             "HULLED"
-                             "ROWS"
-                             "SBLOTCH-LMH"
-                             "SBLOTCH-RATING"
+                             ;; "HULLED"
+                             ;; "ROWS"
+                             ;; "SBLOTCH-LMH"
+                             ;; "SBLOTCH-RATING"
                              )
   "List of strings of traits for which the prediction objective was 'multiclass'")
 
@@ -177,24 +179,22 @@
 
 ;;;; ==================================== Functions
 
-;;;; run-all-reports
-;;;; multiplex file-name-parts/stats-calls into experimental runs
-;;;; validate all filename file properties
-;;;; validate all filename statscalls existance/allowableness
-;;;; validate file set geospatial properties match
-;;;; call run-report for each
-
 (defun run-all-reports (&key (show nil))
-  (-<> (filename-parts show)
+  (when show (format t "~&~%In: run-all-reports"))
+  (-<>
+      (filename-parts show)
     (experiment-dictionary <> show)
-    ;; (validate-files <>)
-    ;; (validate-calls <>)
-    ;; (validate-geospatial <>)
-    ;; (run-report <>)
+    (validate-globals <> show)
+    (validate-files <> show)
+    ;; (validate-calls <> show)
+    ;; (validate-geospatial <> show)
+    ;; (run-report <> show)
     ))
 
-(defun filename-parts(&optional (show nil))
+(defun filename-parts (&optional (show nil))
   "Create a list of filename components"
+  (when show
+    (format t "~&~%In: filename-parts"))
   (let ((experiments '()))
     (dolist (trait *traits-reg*)
       ;; build a p list of lists for the experiment
@@ -211,171 +211,189 @@
     ;; clean up and return
     (let ((clean-experiments (remove-duplicates (reverse experiments) :test #'equal)))
       (when show
-          (dolist (i clean-experiments)
-            (format t "~&~A" i)))
+        (dolist (i clean-experiments)
+          (format t "~&~S" i)))
       clean-experiments)))
 
 (defun experiment-dictionary (filename-parts &optional (show nil))
-  "completes the dictionary of experiment definitions"
-  (when show (print "experiment-dictionary"))
-  (let ((experiments '()))
-    (dolist (fp filename-parts)
-      (let* ((objective (first (getf fp :objective)))
-             (models (getf fp :models))
-             (to-append (cond ((string= objective "regression")
-                               (append *stats-reg-describe-true*
-                                       *stats-reg-describe-pred*
-                                       *stats-reg-compare-pred*
-                                       (when (>= (length models) 2)
-                                         *stats-reg-compare-models*)))
-                              ((string= objective "multiclass")
-                               (append *stats-cat-describe-true*
-                                       *stats-cat-describe-pred*
-                                       *stats-cat-compare-pred*
-                                       (when (>= (length models) 2)
-                                         *stats-cat-compare-models*)))
-                              (t (error "Unknown objective: ~A~&" objective)))))
-        (let ((experiment (copy-list fp)))
-          (setf (getf experiment :stats) to-append)
-          (push experiment experiments))))
-    (setf experiments (nreverse experiments))
-    (when show
-      (dolist (exp experiments)
-        (format t "~&~S~%" exp)))
-    experiments))
-
-'((:TRAIT ("HEIGHT-CM") :OBJECTIVE ("regression") :MODELS ("TSAI" "GBM")) (:TRAIT ("BARLEY-WHEAT") :OBJECTIVE ("multiclass") :MODELS ("TSAI" "GBM")))
-
-(defun experiment-dictionary (filename-parts &optional (show nil))
   "Completes the dictionary of experiment definitions"
+  (when show
+    (format t "~&~%In: experiment-dictionary"))
   (let ((completed-experiments '()))
     (dolist (experiment filename-parts)
       (let* ((objective (first (getf experiment :objective)))
              (models (getf experiment :models))
              (selected-tests (cond
-                      ((string= objective "regression")
-                       (append *stats-reg-describe-true*
-                               *stats-reg-describe-pred*
-                               *stats-reg-compare-pred*
-                               (when (>= (length models) 2)
-                                 *stats-reg-compare-models*)))
-                      ((string= objective "multiclass")
-                       (append *stats-cat-describe-true*
-                               *stats-cat-describe-pred*
-                               *stats-cat-compare-pred*
-                               (when (>= (length models) 2)
-                                 *stats-cat-compare-models*)))
-                      (t (error "Unknown objective: ~A" objective))))
+                               ((string= objective "regression")
+                                (append *stats-reg-describe-true*
+                                        *stats-reg-describe-pred*
+                                        *stats-reg-compare-pred*
+                                        (when (>= (length models) 2)
+                                          *stats-reg-compare-models*)))
+                               ((string= objective "multiclass")
+                                (append *stats-cat-describe-true*
+                                        *stats-cat-describe-pred*
+                                        *stats-cat-compare-pred*
+                                        (when (>= (length models) 2)
+                                          *stats-cat-compare-models*)))
+                               (t (error "Unknown objective: ~A" objective))))
              (experiment-plist (copy-list experiment))
              (test-plist (list :tests selected-tests)))
         (push (append experiment-plist test-plist) completed-experiments)))
     (setf completed-experiments (nreverse completed-experiments))
     (when show
       (dolist (i completed-experiments)
-        (format t "~&~S~%" i)))
+        (format t "~&~S" i)))
     completed-experiments))
+
+(defun validate-globals (experiments &optional (show nil))
+  "checks global variables"
+  (if show
+      (format t "~&~%In: validate-globals"))
+  ;; check models
+  (let ((len-mod (length *models*)))
+    (assert (or (= 1 len-mod)
+                (= 2 len-mod))
+        ()
+        "The number of models in *models* must be 1 or 2. Found: ~A" len-mod))
+  ;; check traits
+  (let ((len-traits (+ (length *traits-cat*)
+                       (length *traits-reg*))))
+    (assert (<= 1 len-traits) () "The number of traits (in either *traits-reg* *traits-cat*) must be at least 1"))
+  (let ((stats-all (append *stats-reg-describe-true* *stats-cat-describe-true* *stats-reg-describe-pred* *stats-cat-describe-pred* *stats-reg-compare-pred* *stats-cat-compare-pred* *stats-reg-compare-models* *stats-cat-compare-models*))))
+
+  ;; check that the stats functions in all *stats-...* lists exist
+  (let ((stats-all (append *stats-reg-describe-true*
+                           *stats-cat-describe-true*
+                           *stats-reg-describe-pred*
+                           *stats-cat-describe-pred*
+                           *stats-reg-compare-pred*
+                           *stats-cat-compare-pred*
+                           *stats-reg-compare-models*
+                           *stats-cat-compare-models*)))
+    (dolist (i stats-all)
+      (when (not (fboundp i))
+        (warn "The function ~A must exist" i))))
+  ;; if all pass, then return  unmodified input
+  experiments)
 
 ;;;; ==================================== API
 
-(run-all-reports)
+(run-all-reports :show t)
 
 ;;;; ==================================== FIN
 
 ;;;; ==================================== build
 
+(defun validate-files (experiments &optional show)
+  "for all experiment dictionaries in experiments, ensure the files exist"
+  (let* (())
+    (locals (())
+            (let* ()))))
+
+;;;; run-all-reports
+;;;; multiplex file-name-parts to dictionary
+;;;; stats-calls into experimental run dictionary
+;;;; validate all global settings
+;;;; &&& validate all filename file properties
+;;;; &&& validate all filename statscalls existance/allowableness
+;;;; &&& validate file set geospatial properties match
+;;;; &&& call run-report for each
+
+
+
+
 (when (= 0 1)
   (print  "Begin never execute")
-  (py:pyversion-info)
+
+
+                                        ; set config
+  ;; (py:initialize)
+  ;; (print py4cl2:*config*) ;; WARN: this triggers the company auto complete hang on lab linux
+  ;; py4cl2:*config* ;; WARN: this triggers the company auto complete hang on lab linux
+
+  ;; (setf (config-var pycmd) python3) ; set one field
+
+
+
+                                        ; ensure version and sys.path is same as python in cli
+  (py:pyversion-info)    ; fails if python command is not resolved in system
+  ;; (py:defpymodule "sys" nil :lisp-package "SYS")
+  ;; (py:defpymodule "pprint" nil :lisp-package "PPRINT")
+  ;; (py:pyexec "pprint.pprint(sys.path)")
+
+                                        ; py process hard reset
+  ;; (py:pystop)
+  ;; (py:python-alive-p)
+  ;; (py:pystart)
+                                        ; python imports
+  (py:defpymodule "rasterio" t :lisp-package "RASTERIO") ; drivers: GTiff GPKG
+  ;; (py:defpymodule "geopandas" nil :lisp-package "GEOPANDAS")
+  ;; (py:defpymodule "sklearn" nil :lisp-package "SKLEARN")
+
+                                        ; open the file
+  (defparameter *gtif-true* #P"/home/holdens/tempdata/predictions1percent/height.tiff")
+
+  ;; pythonic: dataset = rasterio.open('some.tif')
+  (defparameter *dataset* (rasterio:open :fp (namestring *gtif-true*)))
+  (print *dataset*)
+  (py:pyslot-list *dataset* ) ; ie. show me what data it has
+  ;; => ("__class__" "__dict__" "__doc__" "__firstlineno__" "__module__" "__pyx_vtable__" "__static_attributes__" "__weakref__" "_block_shapes" "_closed" "_count" "_crs" "_crs_wkt" "_descriptions" "_dtypes" "_env" "_gcps" "_nodatavals" "_offsets" "_rpcs" "_scales" "_transform" "_units" "block_shapes" "bounds" "closed" "colorinterp" "compression" "count" "crs" "descriptions" "driver" "dtypes" "files" "gcps" "height" "indexes" "interleaving" "is_tiled" "mask_flag_enums" "meta" "mode" "name" "nodata" "nodatavals" "offsets" "options" "photometric" "profile" "res" "rpcs" "scales" "shape" "subdatasets" "transform" "units" "width")
+  (py:pymethod-list *dataset*) ; ie show me what methods it has
+  ;; => ("__class__" "__delattr__" "__dir__" "__enter__" "__eq__" "__exit__" "__format__" "__ge__" "__getattribute__" "__getstate__" "__gt__" "__hash__" "__init__" "__init_subclass__" "__le__" "__lt__" "__ne__" "__new__" "__reduce__" "__reduce_ex__" "__repr__" "__setattr__" "__setstate__" "__sizeof__" "__str__" "__subclasshook__" "_get_crs" "_get_rpcs" "_handle_crswkt" "_has_band" "_has_gcps_or_rpcs" "_mask_flags" "_read" "_set_all_descriptions" "_set_all_offsets" "_set_all_scales" "_set_all_units" "_set_attrs_from_dataset_handle" "_set_crs" "_set_gcps" "_set_nodatavals" "_set_rpcs" "block_size" "block_window" "block_windows" "checksum" "close" "colormap" "dataset_mask" "get_gcps" "get_nodatavals" "get_tag_item" "get_transform" "index" "lnglat" "overviews" "read" "read_crs" "read_masks" "read_transform" "sample" "start" "statistics" "stats" "stop" "tag_namespaces" "tags" "window" "window_bounds" "window_transform" "write_transform" "xy")
+
+                                        ; operations on geotiff
+;;;; slot actions
+  ;; dataset.name
+  (py:pyslot-value *dataset* 'name)
+  ;; dataset.count
+  (py:pyslot-value *dataset* 'count)
+  ;; dataset.dtypes
+  (py:pyslot-value *dataset* 'dtypes)
+  ;; dataset.crs
+  (py:pyslot-value *dataset* 'crs)
+  ;; dataset.shape
+  (py:pyslot-value *dataset* 'shape)
+  ;; dataset-bounds
+  (py:pyslot-value *dataset* 'bounds)
+
+;;;; method actions
+  ;; get nodata value
+  (py:pymethod *dataset* 'get_nodatavals) ; works
+
+  ;; get first band
+  ;; pythonic: dataset.read(1)
+  (py:pymethod *dataset* "read" 1)
+  ;; pythonic: read1 = dataset.read(1)
+  (defparameter *read1* (py:pymethod *dataset* "read" 1))
+  ;; close method when done &&&
+  ;; (py:pymethod *dataset* 'close)
+
+;;;; simple array operations
+  (make-array '(2 2))
+  (make-array '(2 2) :initial-element nil)
+  (make-array '(2 2) :initial-contents '((1 2) (3 4)))
+
+  (defparameter *test-array* (make-array '(2 2) :initial-element 4))
+  (defparameter *test-tens* (make-array '(2 2 2) :initial-element 8))
+  (describe *test-array*)
+  (type-of *test-array*)
+
+  (array-dimensions *test-array*)
+  (aref *test-array* 0 1)
+  (aref *test-tens* 0 0 0)
+  *test-array*
+
+  (type-of *read1*) ;; => (SIMPLE-ARRAY SINGLE-FLOAT (1000 26500))
+  (array-dimensions *read1*) ;; => (1000 26500)
+  (array-total-size *read1*) ;; => 26500000 (25 bits, #x1945BA0, #o145055640, #b1100101000101101110100000)
+  (inspect *read1*) ;; works
+  (describe *read1*) ;; works
+
   (print  "End never execute"))
 
-;; (when (= 0 1)
-;;   (print  "Begin never execute")
 
-
-;;                                         ; set config
-;;   ;; (py:initialize)
-;;   ;; (print py4cl2:*config*) ;; WARN: this triggers the company auto complete hang on lab linux
-;;   ;; py4cl2:*config* ;; WARN: this triggers the company auto complete hang on lab linux
-
-;;   ;; (setf (config-var pycmd) python3) ; set one field
-
-
-
-;;                                         ; ensure version and sys.path is same as python in cli
-;;   (py:pyversion-info)    ; fails if python command is not resolved in system
-;;   ;; (py:defpymodule "sys" nil :lisp-package "SYS")
-;;   ;; (py:defpymodule "pprint" nil :lisp-package "PPRINT")
-;;   ;; (py:pyexec "pprint.pprint(sys.path)")
-
-;;                                         ; py process hard reset
-;;   ;; (py:pystop)
-;;   ;; (py:python-alive-p)
-;;   ;; (py:pystart)
-;;                                         ; python imports
-;;   (py:defpymodule "rasterio" t :lisp-package "RASTERIO") ; drivers: GTiff GPKG
-;;   ;; (py:defpymodule "geopandas" nil :lisp-package "GEOPANDAS")
-;;   ;; (py:defpymodule "sklearn" nil :lisp-package "SKLEARN")
-
-;;                                         ; open the file
-;;   (defparameter *gtif-true* #P"/home/holdens/tempdata/predictions1percent/height.tiff")
-
-;;   ;; pythonic: dataset = rasterio.open('some.tif')
-;;   (defparameter *dataset* (rasterio:open :fp (namestring *gtif-true*)))
-;;   (print *dataset*)
-;;   (py:pyslot-list *dataset* ) ; ie. show me what data it has
-;;   ;; => ("__class__" "__dict__" "__doc__" "__firstlineno__" "__module__" "__pyx_vtable__" "__static_attributes__" "__weakref__" "_block_shapes" "_closed" "_count" "_crs" "_crs_wkt" "_descriptions" "_dtypes" "_env" "_gcps" "_nodatavals" "_offsets" "_rpcs" "_scales" "_transform" "_units" "block_shapes" "bounds" "closed" "colorinterp" "compression" "count" "crs" "descriptions" "driver" "dtypes" "files" "gcps" "height" "indexes" "interleaving" "is_tiled" "mask_flag_enums" "meta" "mode" "name" "nodata" "nodatavals" "offsets" "options" "photometric" "profile" "res" "rpcs" "scales" "shape" "subdatasets" "transform" "units" "width")
-;;   (py:pymethod-list *dataset*) ; ie show me what methods it has
-;;   ;; => ("__class__" "__delattr__" "__dir__" "__enter__" "__eq__" "__exit__" "__format__" "__ge__" "__getattribute__" "__getstate__" "__gt__" "__hash__" "__init__" "__init_subclass__" "__le__" "__lt__" "__ne__" "__new__" "__reduce__" "__reduce_ex__" "__repr__" "__setattr__" "__setstate__" "__sizeof__" "__str__" "__subclasshook__" "_get_crs" "_get_rpcs" "_handle_crswkt" "_has_band" "_has_gcps_or_rpcs" "_mask_flags" "_read" "_set_all_descriptions" "_set_all_offsets" "_set_all_scales" "_set_all_units" "_set_attrs_from_dataset_handle" "_set_crs" "_set_gcps" "_set_nodatavals" "_set_rpcs" "block_size" "block_window" "block_windows" "checksum" "close" "colormap" "dataset_mask" "get_gcps" "get_nodatavals" "get_tag_item" "get_transform" "index" "lnglat" "overviews" "read" "read_crs" "read_masks" "read_transform" "sample" "start" "statistics" "stats" "stop" "tag_namespaces" "tags" "window" "window_bounds" "window_transform" "write_transform" "xy")
-
-;;                                         ; operations on geotiff
-;; ;;;; slot actions
-;;   ;; dataset.name
-;;   (py:pyslot-value *dataset* 'name)
-;;   ;; dataset.count
-;;   (py:pyslot-value *dataset* 'count)
-;;   ;; dataset.dtypes
-;;   (py:pyslot-value *dataset* 'dtypes)
-;;   ;; dataset.crs
-;;   (py:pyslot-value *dataset* 'crs)
-;;   ;; dataset.shape
-;;   (py:pyslot-value *dataset* 'shape)
-;;   ;; dataset-bounds
-;;   (py:pyslot-value *dataset* 'bounds)
-
-;; ;;;; method actions
-;;   ;; get nodata value
-;;   (py:pymethod *dataset* 'get_nodatavals) ; works
-
-;;   ;; get first band
-;;   ;; pythonic: dataset.read(1)
-;;   (py:pymethod *dataset* "read" 1)
-;;   ;; pythonic: read1 = dataset.read(1)
-;;   (defparameter *read1* (py:pymethod *dataset* "read" 1))
-;;   ;; close method when done &&&
-;;   ;; (py:pymethod *dataset* 'close)
-
-;; ;;;; simple array operations
-;;   (make-array '(2 2))
-;;   (make-array '(2 2) :initial-element nil)
-;;   (make-array '(2 2) :initial-contents '((1 2) (3 4)))
-
-;;   (defparameter *test-array* (make-array '(2 2) :initial-element 4))
-;;   (defparameter *test-tens* (make-array '(2 2 2) :initial-element 8))
-;;   (describe *test-array*)
-;;   (type-of *test-array*)
-
-;;   (array-dimensions *test-array*)
-;;   (aref *test-array* 0 1)
-;;   (aref *test-tens* 0 0 0)
-;;   *test-array*
-
-;;   (type-of *read1*) ;; => (SIMPLE-ARRAY SINGLE-FLOAT (1000 26500))
-;;   (array-dimensions *read1*) ;; => (1000 26500)
-;;   (array-total-size *read1*) ;; => 26500000 (25 bits, #x1945BA0, #o145055640, #b1100101000101101110100000)
-;;   (inspect *read1*) ;; works
-;;   (describe *read1*) ;; works
-
-;;   (print  "End never execute"))
+;;;; ==================================== scratch
 ;;;; ==================================== reference
 
 #|
